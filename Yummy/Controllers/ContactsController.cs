601a0f5bbbfc2d1302ee54d6a8e813320480a04f
ibrahim <ibrah@SARIASLAN>
2025-04-01 22:30:02 +0300

    .

---
+++ Yummy/Controllers/ContactsController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Yummy.Context;
+using Yummy.Dtos.ContactDto;
+using Yummy.Entities;
+
+namespace Yummy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public ContactsController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult ContactList()
+        {
+            var values = _context.Contacts.ToList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateContact(CreateContactDto createContactDto)
+        {
+            Contact contact = new Contact();
+            contact.Phone = createContactDto.Phone;
+            contact.Email = createContactDto.Email;
+            contact.Address = createContactDto.Address;
+            contact.MapLocation = createContactDto.MapLocation;
+            contact.OpenHours = createContactDto.OpenHours;
+            _context.Contacts.Add(contact);
+            _context.SaveChanges();
+            return Ok("Ekleme işlemi başarılı");
+        }
+        [HttpDelete]
+        public IActionResult DeleteContact(int id)
+        {
+            var value = _context.Contacts.Find(id);
+            _context.Contacts.Remove(value);
+            _context.SaveChanges();
+            return Ok("Silme işlemi başarılı");
+        }
+        [HttpGet("GetContact")]
+        public IActionResult GetContact(int id)
+        {
+            var value = _context.Contacts.Find(id);
+            return Ok(value);
+        }
+        [HttpPut]
+        public IActionResult UpdateContact(UpdateContactDto updateContactDto)
+        {
+            Contact contact = new Contact();    
+            contact.ContactId = updateContactDto.ContactId;
+            contact.Phone = updateContactDto.Phone;
+            contact.Email = updateContactDto.Email;
+            contact.Address = updateContactDto.Address; 
+            contact.MapLocation= updateContactDto.MapLocation;
+            contact.OpenHours= updateContactDto.OpenHours;
+            _context.Contacts.Update(contact);
+            _context.SaveChanges();
+            return Ok("Güncelleme işlemi başarılı");
+               
+        }
+    }
+}
---
+++ Yummy/Mapping/GeneralMapping.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Yummy.Dtos.FeatureDto;
+using Yummy.Dtos.MessageDto;
+using Yummy.Entities;
+
+namespace Yummy.Mapping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+            //Feature
+            CreateMap<Feature, ResultFeatureDto>().ReverseMap();
+            CreateMap<Feature, CreateFeatureDto>().ReverseMap();
+            CreateMap<Feature, UpdateFeatureDto>().ReverseMap();
+            CreateMap<Feature, GetByIdFeatureDto>().ReverseMap();
+
+            //message
+            CreateMap<Message, ResultMessageDto>().ReverseMap();
+            CreateMap<Message, CreateMessageDto>().ReverseMap();
+            CreateMap<Message, UpdateMessageDto>().ReverseMap();
+            CreateMap<Message, GetByIdMessageDto>().ReverseMap();
+        }
+    }
+}
