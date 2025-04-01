4e241afe20ffc6a267565c769d9bdc621f59728a
ibrahim <ibrah@SARIASLAN>
2025-04-01 23:50:04 +0300

    UI

---
+++ Yummy.WebUI/ViewComponents/_NavbarDefaultComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Yummy.WebUI.ViewComponents
+{
+    public class _NavbarDefaultComponentPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
