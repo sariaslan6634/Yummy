2d7f1ea739fdf5244a5dc06a770a747a9345ea81
ibrahim <ibrah@SARIASLAN>
2025-04-12 20:04:28 +0300

    TESTIMONIAL

--- Yummy.WebUI/ViewComponents/_TestimonialDefaultComponentPartial.cs
+++ Yummy.WebUI/ViewComponents/_TestimonialDefaultComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Yummy.WebUI.Dtos.TestimonialDto;
 
 namespace Yummy.WebUI.ViewComponents
 {
     public class _TestimonialDefaultComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TestimonialDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7295/api/Testimonial");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
